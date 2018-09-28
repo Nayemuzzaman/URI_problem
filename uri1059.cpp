769e509dd2f7e8f8af14c4560c79c83454f651cd
Nayemuzzaman <Nayemuzzaman@users.noreply.github.com>
2018-09-28 10:34:45 +0600

    URI 1059-even number

---
+++ uri1059.cpp
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i=1, l=100;
+
+    for(i=1;i<=100;i++){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+        else{
+
+        }
+    }
+}
