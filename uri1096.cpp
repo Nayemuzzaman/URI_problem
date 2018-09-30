838c4e702762b8cd498cbc6a904f13bf92934f85
Nayemuzzaman <Nayemuzzaman@users.noreply.github.com>
2018-10-01 00:28:04 +0600

    uri 1096

---
+++ uri1096.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+
+/**
+* Problem No: URI Online Judge Problem 1096
+* Solution in C++ language
+* Author: Nayemuzzaman
+**/
+
+
+using namespace std;
+
+int main(){
+    int i,j;
+    for(i=1;i<10;i++){
+        for(j=7;j>4;j--){
+           cout << "I=" << i << " " << "J=" << j << "\n";
+
+        }
+        i++;
+    }
+}
