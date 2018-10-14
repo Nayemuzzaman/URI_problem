fc3ff17b8ad665450ee696a741ccf5490d8f0a16
Nayemuzzaman <Nayemuzzaman@users.noreply.github.com>
2018-10-15 00:44:20 +0600

    Update uri 1142

---
+++ uri1142.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stdio.h>
+
+
+/**
+* Problem No: URI Online Judge Problem 1142
+* Solution in C++ language
+* Author: Nayemuzzaman
+**/
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int j=0;
+
+    scanf("%d",&n);
+
+
+        for(int i=0;i<n;i++){
+            int x=0;
+            while(x!=3){
+                    j++;
+                printf("%d ",j);
+            x++;
+            }
+
+            printf("PUM\n");
+            j++;
+        }
+
+}
